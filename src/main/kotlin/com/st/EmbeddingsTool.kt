0e7fbb8bafb235eaedfee0bae3a637c3b747eb48
staillebois <sylvain.taillebois@gmail.com>
2025-02-04 07:43:32 +0100

    Generate embeddings for title and description

--- src/main/kotlin/com/st/EmbeddingsTool.kt
+++ src/main/kotlin/com/st/EmbeddingsTool.kt
@@ -1,9 +1,16 @@
 package com.st
 
+import dev.langchain4j.data.document.Document
+import dev.langchain4j.data.document.DocumentSplitter
 import dev.langchain4j.data.document.Metadata
+import dev.langchain4j.data.document.splitter.DocumentByParagraphSplitter
+import dev.langchain4j.data.embedding.Embedding
 import dev.langchain4j.data.segment.TextSegment
 import dev.langchain4j.model.embedding.EmbeddingModel
 import jakarta.enterprise.context.ApplicationScoped
+import org.jboss.logging.Logger
+import java.lang.String.join
+
 
 @ApplicationScoped
 class EmbeddingsTool(
@@ -29,7 +36,22 @@ class EmbeddingsTool(
             "pubDate", rss.pubDate,
             "category", rss.category
         )
-        val segment = TextSegment(rss.description, Metadata(metas))
-        return embeddingModel.embed(segment).content().vectorAsList()
+        // Join with double lineSeparator (\n\n) in order to leverage DocumentByParagraphSplitter
+        val content: String = join(System.lineSeparator() + System.lineSeparator(),
+            listOf(rss.title,rss.description))
+
+        val document = Document(content, Metadata(metas))
+        val splitter: DocumentSplitter = DocumentByParagraphSplitter(800, 50)
+
+        val segments = splitter.split(document)
+        LOGGER.infof("The document was split as %s chunks", segments.size)
+        val embeddings: List<Embedding> = embeddingModel.embedAll(segments).content()
+        return embeddings.get(0).vectorAsList()
+//        val segment = TextSegment(rss.description, Metadata(metas))
+//        return embeddingModel.embed(segment).content().vectorAsList()
+    }
+
+    companion object {
+        private val LOGGER: Logger = Logger.getLogger(EmbeddingsTool::class.java)
     }
 }
